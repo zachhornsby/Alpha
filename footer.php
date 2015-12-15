c81081bd13144f744901db335db53fdd8cd8dd17
Zach Hornsby <me@zachhornsby.com>
2015-12-14 22:43:23 -0500

    Few small changes

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	<footer class="footer" role="contentinfo">
 
 		<p class="credits">
-			&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
+			&copy;<?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
 			Powered by <a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//github.com/zachhornsby/Alpha" title="Alpha Theme">Alpha</a>.
 		</p>
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,1 @@
-(function ($, root, undefined) {
-	
-	$(function () {
-		
-		'use strict';
-		
-		// DOM ready, take it away
-		
-	});
-	
-})(jQuery, this);
+// Code away
