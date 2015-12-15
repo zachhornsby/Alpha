69fe37870120f052358bca5fd6f93fff4922ab2d
Zach Hornsby <me@zachhornsby.com>
2015-12-14 23:44:59 -0500

    Overhauls index.php

--- index.php
+++ index.php
@@ -1,34 +1,18 @@
 <?php get_header(); ?>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<main role="main">
 
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+		<section>
 
-			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+			<h1><?php _e( 'Latest Posts', 'alpha' ); ?></h1>
 
-			<?php get_template_part( 'inc/meta'); ?>
+			<?php get_template_part('loop'); ?>
 
-			<div class="entry">
-				<?php the_content(); ?>
-			</div>
+			<?php get_template_part('pagination'); ?>
 
-			<div class="postmetadata">
-				<?php the_tags('Tags: ', ', ', '<br />'); ?>
-				Posted in <?php the_category(', ') ?> | 
-				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-			</div>
+		</section>
 
-		</div>
-
-	<?php endwhile; ?>
-
-	<?php get_template_part( 'inc/nav'); ?>
-
-	<?php else : ?>
-
-		<h2>Not Found</h2>
-
-	<?php endif; ?>
+	</main>
 
 <?php get_sidebar(); ?>
 
