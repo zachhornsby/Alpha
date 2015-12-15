7f63fd81a2361724cf70cddd227eb0a7004416f9
Zach Hornsby <me@zachhornsby.com>
2015-12-14 22:06:27 -0500

    Overhauls footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
+	<footer class="footer" role="contentinfo">
+
+		<p class="credits">
+			&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
+			Powered by <a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//github.com/zachhornsby/Alpha" title="Alpha Theme">Alpha</a>.
+		</p>
+
+	</footer>
+
+</div> <!-- /wrapper -->
+
 <?php wp_footer(); ?>
-	
+
 </body>
 </html>
