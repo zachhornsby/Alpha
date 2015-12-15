409974e7dc9ef0dba050279e9191df07f6d037d0
Zach Hornsby <me@zachhornsby.com>
2015-12-14 22:23:28 -0500

    Overhauls 404.php

--- 404.php
+++ 404.php
@@ -1,6 +1,18 @@
 <?php get_header(); ?>
 
-	<h2>Error 404 - Page Not Found</h2>
+	<main role="main">
+		
+		<section>
+
+			<article id="post-404">
+
+				<h1>Page not found</h1>
+				<h2><a href="<?php echo home_url(); ?>">Return home?</a></h2>
+
+			</article>
+
+		</section>
+	</main>
 
 <?php get_sidebar(); ?>
 
